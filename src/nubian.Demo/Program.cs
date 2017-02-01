782f450637caa88ab4fdf8297c7dc5acb77f5f28
Jacob Reimers <jacob@reimers.dk>
2017-02-01 18:37:30 +0100

    Add demo project.

---
+++ src/nubian.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace nubian.Demo
+{
+    using NEventStore;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = Wireup.Init()
+                .UsingInMemoryPersistence()
+                .UsingJsonSerialization()
+                .Build();
+
+            store.Dispose();
+        }
+    }
+}
